46d05aa9a957d875d5d0552d9ed24494c8c208fa
Blake Sager <bsager@atlassian.com>
2022-11-26 12:34:26 -0600

    Finish video 15

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,48 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    console.log(fish)
+    const fishes = {...this.state.fishes}
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes
+    })
+  }
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" age={100} />
+          <ul className="fishes">
+            { 
+              Object
+                .keys(this.state.fishes)
+                .map(f => <Fish key={f} details={this.state.fishes[f]}/>) 
+            }
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,26 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  // define a property, not a method, to fix `this` references
+  goToStore = (event) => {
+    event.preventDefault()
+    const store = this.myInput.current.value;
+    this.props.history.push(`/store/${store}`)
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input 
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
